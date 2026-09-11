677afb680035e9aea45d63568216bb228eb57470
DarkJutsu <ulloajose34@gmail.com>
2026-09-10 22:01:54 -0600

    feat:add @RestQuery for interactive app

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -4,13 +4,19 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.jboss.resteasy.reactive.RestQuery;
 
 @Path("/hello")
 public class GreetingResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from Quarkus REST";
+    /**
+     * Returns a greeting message based on the provided name.
+     * @param name The name to be included in the greeting message.
+     * @return A greeting message in plain text format.
+     */
+    @GET // HTTP GET method
+    @Produces(MediaType.TEXT_PLAIN) // Produces plain text response
+    public String hello(@RestQuery String name) { // @RestQuery - Binds the query parameter 'name' from the request URL to the method parameter
+        return "Hello " + name;
     }
 }
